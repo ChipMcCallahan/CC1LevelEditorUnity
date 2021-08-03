e07ef5d3730766e00a5241e5648ac4336e4f225b
ChipMcCallahan <thisischipmccallahan@gmail.com>
2021-08-02 22:29:41 -0700

    Add NuGet and Protobuf packages; basic protobuf parsing

--- Assets/FileLister.cs
+++ Assets/FileLister.cs
@@ -2,18 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using p = Cc1LevelsetUtils;
 
 public class FileLister : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        Dictionary<string, p.Levelset> sets = new Dictionary<string, p.Levelset>();
         string cwd = Directory.GetCurrentDirectory();
         foreach (string path in Directory.GetFiles(cwd + "\\Assets\\cc1bin\\")) {
             if (path.EndsWith(".bin")) {
-                Debug.Log(path);
+                using (var input = File.OpenRead(path))
+                {
+                    sets.Add(path, p.Levelset.Parser.ParseFrom(input));
+                }
             } 
         }
+
+        foreach(KeyValuePair<string, p.Levelset> entry in sets)
+        {
+            Debug.Log(entry.Key);
+            Debug.Log(entry.Value.Levels.Count);
+            // do something with entry.Value or entry.Key
+        }
+
     }
 
     // Update is called once per frame
